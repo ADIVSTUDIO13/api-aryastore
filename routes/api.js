1daf6c00c7f9c3d1d12cd29c52c6a3d6f0f7b814
ADIVSTUDIO13 <154042827+ADIVSTUDIO13@users.noreply.github.com>
2024-12-17 17:29:04 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -28,21 +28,31 @@ res.json(loghandler.notapikey)
 }
 })
 router.get('/downloader/tiktok', async (req, res, next) => {
-var url = req.query.url
-var apikey = req.query.apikey
-if (!url) return res.json(loghandler.noturl)
-if (!apikey) return res.json(loghandler.notapikey)
-if(listkey.includes(apikey)){
-let anu = await fetchJson(`https://api.botcahx.biz.id/api/dowloader/tikok?url=${url}&apikey=Admin`)
-res.json({
-status: true,
-creator: `${creator}`,
-result: anu.result
-})
-} else {
-res.json(loghandler.notapikey)
-}
-})
+    var url = req.query.url || 'https://vt.tiktok.com/ZS6dfAr5B/'; // URL default
+    var apikey = req.query.apikey;
+
+    if (!apikey) return res.json(loghandler.notapikey);
+
+    if (listkey.includes(apikey)) {
+        try {
+            let anu = await fetchJson(`https://api.lolhuman.xyz/api/tiktok?url=${url}&apikey=b9972cae27237ab59e8aa1a6`);
+            res.json({
+                status: true,
+                creator: `${creator}`,
+                result: anu.result
+            });
+        } catch (err) {
+            res.json({
+                status: false,
+                message: 'Error fetching data',
+                error: err.message
+            });
+        }
+    } else {
+        res.json(loghandler.notapikey);
+    }
+});
+
 router.get('/downloader/ytplay', async (req, res, next) => {
 var q = req.query.q
 var apikey = req.query.apikey
@@ -1310,12 +1320,25 @@ var apikey = req.query.apikey
 if (!url) return res.json(loghandler.noturl)
 if (!apikey) return res.json(loghandler.notapikey)
 if(listkey.includes(apikey)){
-let result = await getBuffer(`https://saipulanuar.ga/api/download/ssweb?url=${url}`)
+let result = await getBuffer(`https://api.lolhuman.xyz/api/ssweb?url=${url}&apikey=9b982a9592d5b0836042669b`)
 res.set({'Content-Type': 'image/jpg'})
 res.send(result)
 } else {
 res.json(loghandler.notapikey)
 }
 })
-
+- tools entertainet
+// router.get('/tools/family100' , async (req, res, next) => {
+//    var question = req.question || 'berapa presiden di indonesia?';
+//   var apikey = req.query.apikey;
+//   if (!question) return res.json(loghandler.notquestion)
+//   if (!apikey) return res.json(loghandler.notapikey)
+//   if(listkey.includes(apikey)){
+//   let result = await getBuffer(`https://api.lolhuman.xyz/api/tebak/family100?question=${question}&apikey=${lolkey}`)
+// res.set({'Content-Type': 'text/plain'});
+// res.send(result)
+// } else {
+// res.json(loghandler.notapikey)
+// }
+// })  
 module.exports = router
